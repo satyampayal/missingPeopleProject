b5b18cda73fb0a7fff8393b1581c8de9c73f80df
Satyam Payal <122890312+satyampayal@users.noreply.github.com>
2023-09-29 09:53:51 +0530

    Delete Desktop directory

--- Desktop/MissingPeopeProject/backend/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { configDotenv } from "dotenv";
-configDotenv();
-import connectToDB from "./dbConfig/db.js";
-const port=process.env.PORT;
-connectToDB(process.env.DB_URL);
-
-app.listen(port,()=>console.log(`App is listen at ${port}`));
-
--- Desktop/MissingPeopeProject/backend/routes/routes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import {registeruser} from '../controllers/routeControllers'
-const route=express.Router();
-
-route.post('/register',registeruser);
